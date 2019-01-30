e73654b034ea09256f4af45d0d5e96b6bc84a19a
Getulio Vieira <getuliovdmnaweb@gmail.com>
2019-01-29 22:45:50 -0300

    Add Customer - Adding Service Layer to Project.

--- src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -15,18 +15,29 @@ public class CustomerDAOImpl implements CustomerDAO {
 	@Autowired
 	private SessionFactory sessionFactory;
 	@Override
-	@Transactional
+	
 	public List<Customer> getCustomers() {
 		// get the curent Hibernate Sesson
 		Session currentSession = sessionFactory.getCurrentSession();
-		// create a query
+		// create a query .. sort by last name
 		Query<Customer> theQuery =
-				currentSession.createQuery("from Customer", Customer.class);
+				currentSession.createQuery("from Customer order by lastName", Customer.class);
 		// execute query and get result list
 		List<Customer> customers = theQuery.getResultList();
 		// return results
 		
 		return customers;
+	}
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+	
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		currentSession.save(theCustomer);
+		// save the customer... finally LoL
+		
+		
 	}
 
 }
